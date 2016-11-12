66254e8956d5006fa95ffe1b31d5e0d96846cfb5
taldy <taldy@ya.ru>
2016-11-12 03:12:37 +0200

    Remove console logs

--- content.js
+++ content.js
@@ -10,7 +10,6 @@
   ////////////////
 
   function activate() {
-    console.log('this is a content.js #2', jQuery);
     registerPomodoroHandlers();
   }
 
@@ -21,21 +20,11 @@
   }
 
   function onTimerStart(event) {
-    console.log('onPomodoroStart', event);
-
-    //initialTime = getTimerClockText();
-    //setTimeout(onTimerTick, 1);
     let timerData = getTimerData();
     totalTime = getTotalTime(timerData);
-    console.log('totalTime', totalTime);
     startTime = getStartTime(timerData);
-    console.log('startTime', startTime);
 
     setTimeout(onTimerTick, 1);
-  }
-
-  function onTimerEnd() {
-
   }
 
   function onTimerTick() {
@@ -66,7 +55,6 @@
     }
     catch (e) {}
 
-    console.log('getTimerData', Object.assign({}, data));
     return data;
   }
 
@@ -87,41 +75,20 @@
     return totalTime - getCurrentTime();
   }
 
-  function getClockElement() {
-    let mainClock = jQuery('#workTimerDialog:visible .workTimerDialog-clockText');
-    let smallClock = jQuery('.toolbar-timerButton-hasClockText .toolbar-button-text');
-
-    return mainClock.length ? mainClock : smallClock;
-  }
-
-  function getTimerClockText() {
-    return getClockElement().text();
-  }
-
   function isStarted() {
     return !!localStorage[getTimerPrefix() + 'WorkTimerSession:WorkEntry'];
   }
 
-  function isActive() {
-    return !!jQuery('#workTimerDialog:visible').length;
-  }
-
   function synchronisePomodoro(left, total) {
-    console.log('synchronisePomodoro', left, total);
     chrome.extension.sendMessage({action: 'pomodoroTick', left, total}, () => {});
   }
 
   function synchroniseStopwatch(current) {
-    console.log('synchroniseStopwatch', current);
     chrome.extension.sendMessage({action: 'stopwatchTick', current}, () => {});
   }
 
   function cancel() {
-    console.log('cancel');
     chrome.extension.sendMessage({action: 'cancel'}, () => {});
   }
-
-  //
-  // .toolbar-timerButton-hasClockText .toolbar-button-text
 
 })();
